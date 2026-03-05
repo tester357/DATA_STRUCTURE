168b839d253aba3c7570b9c218578eda833f80ff
Youssef <Youssef>
2026-03-05 21:55:34 +0200

    test Node andsingly linked list

---
+++ test/test/test.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+#define SIZE 5
+typedef int Entry;
+
+struct Queue {
+    int front;
+    int rear;
+    int count;
+    Entry arr[SIZE];
+};
+
+void QueueTraverse(Queue* pq, void (*pf)(Entry))
+{
+    int pos = pq->front;
+
+    for (int i = 0; i < pq->count; i++)
+    {
+        pf(pq->arr[pos]);
+        pos = (pos + 1) % SIZE;
+    }
+}
+
+void Print(Entry e)
+{
+    cout << e << " ";
+}
+
+int main()
+{
+    Queue q;
+
+    q.front = 0;
+    q.count = 3;
+
+    q.arr[0] = 10;
+    q.arr[1] = 20;
+    q.arr[2] = 30;
+
+    QueueTraverse(&q, Print);
+
+}
