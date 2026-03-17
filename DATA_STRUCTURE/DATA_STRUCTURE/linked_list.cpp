f52160a346c69fd6f90a3d3675bcf9d42a018b80
youssef_ahmed <youssefahmed2005ah@gmail.com>
2026-03-17 02:31:12 +0200

    Implement Display and Delet_BYvalue functions
    Added Display and Delet_BYvalue functions to manage linked list.

--- DATA_STRUCTURE/DATA_STRUCTURE/linked_list.cpp
+++ DATA_STRUCTURE/DATA_STRUCTURE/linked_list.cpp
@@ -18,10 +18,62 @@ void insert_last(int value)
     else
     {
         node *sh=head;
-        while (head->next!=NULL)
+        while (sh->next!=NULL)
         {
           sh=sh->next;
         }
         sh->next=arr;
     }
+}
+void Display()
+{
+    node* sho=head;
+    if(head==NULL)
+    {
+        cout<<"IS EMPTY\n";
+    }
+    else
+    {
+        while(sho!=NULL)
+        {
+            cout<<sho->data<<"\t";
+            sho=sho->next;
+        }
+    }
+
+}
+void Delet_BYvalue(int value)
+{
+    node*first=head,*last=NULL;
+    if(head==NULL)
+    {
+        cout<<"IS EMPTY\n";
+    }
+    else if(head->data==value)
+    {
+        head=head->next;
+        delete first;
+        return ;
+    }
+    else
+    {
+        while(first!=NULL)
+        {
+            last=first;
+            last->next=first->next;
+            delete first;
+
+        }
+    }
+}
+int main()
+{
+    insert_last(3);
+    insert_last(5);
+    insert_last(9);
+    insert_last(1);
+    Display();
+    cout<<"\n";
+    Delet_BYvalue(3);
+    Display();
 }
